840244e8dd889d943b2fd1eb8522cd6d8bc93b06
Zayan-Husain <zayan@banogroup.net>
2020-05-26 13:04:22 -0400

    dash and dbl jump

--- js/entities/player.js
+++ js/entities/player.js
@@ -5,10 +5,10 @@ class player extends yentity {
     this.speed = 5;
     this.type = "player";
     this.grafic_type = "none";
-    this.speed = 3;
+    this.speed = 1.5;
     this.speedx = 0;
     this.speedy = 0;
-    this.gravity = 7.6;
+    this.gravity = 4.6;
     this.vf = 0.5;
     this.hf = 0.55;
     this.original_gravity = this.gravity;
@@ -16,40 +16,54 @@ class player extends yentity {
 
     this.on_ground;
     this.jump_power = -170;
+    this.jumps = 0;
+    this.max_jumps = 9;
+    this.dash_count = 0;
+    this.max_dash_count = 2;
+    this.dash_power = 50;
+    this.dash_left = 0;
+    this.dash_right = 0;
+    this.dir = "";
+    this.left_dash_counter;
+    this.right_dash_counter;
   } //end constructor
 
   update() {
     var t = this;
     super.update();
-	t.reset_btn();
+    t.reset_btn();
     t.move();
     t.adjustPosX();
     t.adjustPosY();
     t.camera_control();
+    t.dash();
   } //end update
   move() {
     var t = this;
     var g = t.hit_test("tile", 0, 1); //collide ground from bottom
     if (keyDown("A")) {
       t.speedx -= t.speed;
+      this.dir = "left";
     }
     if (keyDown("D")) {
       t.speedx += t.speed;
+      this.dir = "right";
     }
 
     if (keyWentUp("A") || keyWentUp("D")) {
       t.speedx = 0;
     }
-
     if (g) {
       t.speedy = 0;
+      t.jumps = 0;
       t.on_ground = true;
-      if (keyDown("SPACE")) {
-        t.speedy += this.jump_power;
-      }
     } else {
       t.speedy += t.gravity;
       t.on_ground = false;
+    }
+    if (keyWentUp("SPACE") && t.jumps < t.max_jumps) {
+      t.speedy += this.jump_power;
+      t.jumps++;
     }
     t.speedy *= t.vf;
     t.speedx *= t.hf;
@@ -93,15 +107,25 @@ class player extends yentity {
     camera.position.x = t.x;
     camera.position.y = t.y;
   } //end camera_control
-  
-  reset_btn()
-  {
-	  var t=this;
-	  if (keyWentUp("R")) 
-	  {
-		  console.log("reset game");
-		  t.world.init();
-	  }
-  }//end reset_btn
+
+  reset_btn() {
+    var t = this;
+    if (keyWentUp("R")) {
+      console.log("reset game");
+      t.world.init();
+    }
+  } //end reset_btn
+  dash() {
+    if (keyWentUp("A")) {
+      this.dash_left++;
+    }
+    if (keyWentUp("D")) {
+      this.dash_right++;
+    }
+    if (this.dash_right >= this.max_dash_count) {
+      this.dash_right = 0;
+      this.speedx += this.dash_power;
+    }
+  }
 } //end class
 ///////////////end player///////////////////
--- js/worlds/game_world.js
+++ js/worlds/game_world.js
@@ -1,37 +1,22 @@
 class game_world extends world {
   constructor(name2, wh2) {
     super(name2);
-    this.level_tst = [
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-      [1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1],
-      [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-      [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-      [1, 0, 0, 3, 0, 0, 1, 1, 0, 0, 1],
-      [1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
-      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ];
+    this.level_tst = ymaps.level_tst;
     this.level_1 = ymaps.level_1;
     this.wh = wh2;
   }
 
   init() {
     var t = this;
-    t.resetw();//reset world
+    t.resetw(); //reset world
     var tmap = new tilemap(this.level_1);
     t.add(tmap);
 
     tmap.draw_map();
   }
-  
-  update()
-  {
-	  var t=this;
-	  super.update();
-	  
-  }
-  
 
+  update() {
+    var t = this;
+    super.update();
+  }
 }
